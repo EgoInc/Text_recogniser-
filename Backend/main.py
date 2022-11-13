809bebbb15e9e603dae6dbbf4a1f2c2cb912cef7
anastasia.lavrova <anastasia.lavrova@lanit.tercom.com>
2022-11-13 20:53:31 +0300

    Division

--- Backend/main.py
+++ Backend/main.py
@@ -4,17 +4,26 @@ import PIL.Image
 import cv2
 def checkWord(word):
     for char in word:
+
         if not char.isalpha():
-            return False
+            if char=="!":
+                pass
+            else:
+                return False
     return True
 
 
-myconfig = r"-l eng  --oem 3"
-img_path = "./Test Images/sings.jpg"
+
+param = '2'
+lang = 'rus'
+myconfig = r"-l {} --psm 11 --oem 3".format(lang)
+img_path = "./Test Images/ad.jpg"
+
 
 text = pytesseract.image_to_string(PIL.Image.open(img_path),
                                    config = myconfig)
 print(text)
+
 
 
 """__________________________________"""
@@ -34,12 +43,14 @@ amount_boxes =  len(data['text'])
 for i in range(amount_boxes):
     text = data['text'][i]
     if checkWord(text) and text!="":
-        if float(data['conf'][i]) > 60:
+        if float(data['conf'][i]) > 70:
             print(text)
             (x, y, width, height) = (data['left'][i], data['top'][i],  data['width'][i], data['height'][i])
             img = cv2.rectangle(img, (x,y), (x+width, y+height), (255, 20, 100), 2)
-            img = cv2.putText(img, text, (x, y+height+20), cv2.FONT_HERSHEY_COMPLEX, 0.7, (150,30, 180), 2)
+            img = cv2.putText(img, text, (x, y+height+20), cv2.FONT_HERSHEY_COMPLEX, 0.7, (255 ,255 , 0), 2)
 
 
 cv2.imshow("Detected_Words", img)
 cv2.waitKey(0)
+
+
